3484317de9b3df816ed709611c3c607079d3903c
mainakdas195 <65386328+mainakdas195@users.noreply.github.com>
2022-01-17 14:12:47 +0530

    Add files via upload

--- src/main/java/com/example/CustomerService/model/InputOrder.java
+++ src/main/java/com/example/CustomerService/model/InputOrder.java
@@ -10,7 +10,6 @@ import javax.validation.constraints.NotNull;
 
 import org.springframework.validation.annotation.Validated;
 
-import com.example.demo.base.model.InputOrderProduct;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import io.swagger.annotations.ApiModelProperty;
