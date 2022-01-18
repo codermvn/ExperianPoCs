661a3eec8407b4da2c88806e8f41c1a243f1d97e
mainakdas195 <65386328+mainakdas195@users.noreply.github.com>
2022-01-18 12:01:01 +0530

    Add files via upload

--- src/main/java/com/example/CustomerService/util/CustomerValidator.java
+++ src/main/java/com/example/CustomerService/util/CustomerValidator.java
@@ -13,7 +13,8 @@ public class CustomerValidator {
 	
 	public static void validateCustomer(InputCustomer inputCustomer) {
 		if (!Optional.ofNullable(inputCustomer.getCustomerName()).isPresent() || !Optional.ofNullable(inputCustomer.getCustomerAddress()).isPresent()
-				|| !Optional.ofNullable(inputCustomer.getDebitAvailable()).isPresent() || !Optional.ofNullable(inputCustomer.getModeOfPayment()).isPresent()) {
+				|| !Optional.ofNullable(inputCustomer.getDebitAvailable()).isPresent() || !Optional.ofNullable(inputCustomer.getModeOfPayment()).isPresent()
+				|| inputCustomer.getDebitAvailable().signum() == 0 || inputCustomer.getDebitAvailable().signum() == -1) {
 			throw new CustomerServiceException(BAD_REQUEST_MESSAGE);
 		}
 	}
@@ -23,7 +24,8 @@ public class CustomerValidator {
 	 * @param customerAmount
 	 */
 	public static void validateAmount(@Valid CustomerAmount customerAmount) {
-		if (!Optional.ofNullable(customerAmount.getValue()).isPresent() || customerAmount.getValue().signum() == -1) {
+		if (!Optional.ofNullable(customerAmount.getValue()).isPresent() || customerAmount.getValue().signum() == -1 || !Optional.ofNullable(customerAmount.getCustomerId()).isPresent() 
+				|| customerAmount.getValue().signum() == 0 || customerAmount.getValue().signum() == -1 ) {
 			throw new CustomerServiceException(BAD_REQUEST_MESSAGE);
 		}
 	}
