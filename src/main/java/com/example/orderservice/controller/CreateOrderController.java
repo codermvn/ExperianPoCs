71972fc4d957e54e82805f83434f75eb923f6459
Shivangi Singh <78526295+Shivangi950@users.noreply.github.com>
2022-01-22 18:54:40 +0530

    Transaction management

--- src/main/java/com/example/orderservice/controller/CreateOrderController.java
+++ src/main/java/com/example/orderservice/controller/CreateOrderController.java
@@ -9,15 +9,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+
 import com.example.orderservice.controllerapi.CreateOrderApi;
-import com.example.orderservice.model.Customer;
 import com.example.orderservice.model.ExceptionResponse;
 import com.example.orderservice.model.InputOrder;
 import com.example.orderservice.model.Order;
 import com.example.orderservice.service.OrderService;
-import com.example.orderservice.transformer.CreateOrderTransformer;
-import com.example.orderservice.util.OrderValidator;
 
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -26,9 +23,6 @@ import io.swagger.annotations.ApiResponses;
 
 @RestController
 public class CreateOrderController implements CreateOrderApi {
-
-	@Autowired
-    private RestTemplate restTemplate;
 	
 	@Autowired
 	OrderService orderService;
@@ -42,13 +36,7 @@ public class CreateOrderController implements CreateOrderApi {
 	public ResponseEntity<Order> createOrderPost(
 			@ApiParam(value = "Order to add") @Valid @RequestBody InputOrder inputOrder) {
 		
-		OrderValidator.validateOrder(inputOrder);
-		
-		Customer customer = restTemplate.getForObject("http://localhost:8082/getCustomer/{customerId}", Customer.class, inputOrder.getCustomerId());
-		
-		Order order = CreateOrderTransformer.getInstance(restTemplate).populateOrder(inputOrder, customer);
-		Order addOrder = orderService.addOrder(order);
-		
-		return new ResponseEntity<>(addOrder, HttpStatus.OK);
+		Order addedOrder = orderService.createOrderWithException(inputOrder);
+		return new ResponseEntity<>(addedOrder, HttpStatus.OK);
 	}
 }
