70a65f32700fe8e084a7405375418138392d1439
Shivangi Singh <78526295+Shivangi950@users.noreply.github.com>
2022-01-22 18:53:36 +0530

    Transaction management

--- src/main/java/com/example/orderservice/config/Config.java
+++ src/main/java/com/example/orderservice/config/Config.java
@@ -1,8 +1,13 @@
-
 package com.example.orderservice.config;
 
+import javax.jms.Queue;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.command.ActiveMQQueue;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -12,4 +17,25 @@ public class Config {
 	public RestTemplate restTemplate() {
 	    return new RestTemplate();
 	}
+	
+	 @Value("${activemq.broker-url}")
+	    private String brokerUrl;
+
+	    @Bean
+	    public Queue queue() {
+	        return new ActiveMQQueue("test.queue");
+	    }
+
+	    @Bean
+	    public ActiveMQConnectionFactory activeMQConnectionFactory() {
+	        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory();
+	        factory.setBrokerURL(brokerUrl);
+	        return factory;
+	    }
+
+	    @Bean
+	    public JmsTemplate jmsTemplate() {
+	        return new JmsTemplate(activeMQConnectionFactory());
+	    }
+
 }
