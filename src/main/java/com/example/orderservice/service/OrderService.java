6524279c61a6a533f5ac62d06d8a6b29535358e0
Shivangi Singh <78526295+Shivangi950@users.noreply.github.com>
2022-01-22 18:59:54 +0530

    Transaction management

--- src/main/java/com/example/orderservice/service/OrderService.java
+++ src/main/java/com/example/orderservice/service/OrderService.java
@@ -1,18 +1,39 @@
 package com.example.orderservice.service;
 
+import javax.jms.Queue;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.client.RestTemplate;
 
 import com.example.orderservice.exception.CustomerNotFoundException;
+import com.example.orderservice.exception.InsufficientBalanceException;
+import com.example.orderservice.model.Customer;
+import com.example.orderservice.model.CustomerAmount;
+import com.example.orderservice.model.InputOrder;
 import com.example.orderservice.model.Order;
 import com.example.orderservice.model.Order.OrdStatusEnum;
 import com.example.orderservice.repository.OrderRepository;
+import com.example.orderservice.transformer.CreateOrderTransformer;
+import com.example.orderservice.util.OrderValidator;
 
 @Service
 public class OrderService {
 
 	@Autowired
 	OrderRepository orderRepository;
+	
+	@Autowired
+	 Queue queue;
+		
+	@Autowired
+	JmsTemplate jmsTemplate;
+
+	@Autowired
+	private RestTemplate restTemplate;
 
 	public Order addOrder(Order order) {
 		Order addedOrder = null;
@@ -23,7 +44,7 @@ public class OrderService {
 		}
 		return addedOrder;
 	}
-	
+
 	public Order getOrderById(int id) {
 		Order order = null;
 		try {
@@ -33,7 +54,7 @@ public class OrderService {
 		}
 		return order;
 	}
-	
+
 	public Order deleteOrderById(int id) {
 		Order deletedOrder = null;
 		try {
@@ -44,5 +65,35 @@ public class OrderService {
 			throw new CustomerNotFoundException("order not found");
 		}
 		return deletedOrder;
+	}
+
+	//@Transactional(rollbackFor = { InsufficientBalanceException.class })
+	public Order createOrderWithException(@RequestBody InputOrder inputOrder)  {
+
+		OrderValidator.validateOrder(inputOrder);
+		Integer customerId = inputOrder.getCustomerId();
+		Customer customer = restTemplate.getForObject("http://localhost:8082/getCustomer/{customerId}", Customer.class,
+				customerId);
+
+		Order order = CreateOrderTransformer.getInstance(restTemplate).populateOrder(inputOrder, customer);
+		Order addOrder = addOrder(order);
+
+		CustomerAmount customerAmount = new CustomerAmount();
+		customerAmount.setCustomerId(customerId);
+		customerAmount.setValue(order.getOrderValue());
+
+		/**
+		 * below will debit amount from customer.
+		 */
+		try {
+			Customer updatedCustomer = restTemplate.postForObject("http://localhost:8082/debit", customerAmount,
+					Customer.class);
+		} catch (Exception e) {
+			System.out.println("insufficient balance to book order");
+			jmsTemplate.convertAndSend(queue,order.getOrderId());
+			order.setOrdStatus(OrdStatusEnum.FAILED);
+			throw new InsufficientBalanceException("balance insufficient");
+		}
+		return addOrder;
 	}
 }
