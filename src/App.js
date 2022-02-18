d5f696f6896a3f33ab01119523dce7399983d52f
mainakdas195 <65386328+mainakdas195@users.noreply.github.com>
2022-02-18 11:32:10 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import HeaderList from './HeaderList';
+import SlickDemo from './SlickDemo';
+import SlideShow from "./SlideShow";
+import Footer from './Footer';
+
+import './Styles.css';
+import Header from './Header';
+
+import Product from './Product';
+
+function App() {
+  return (
+    <div>
+       <Product/>
+    </div>  
+  );
+}
+
+export default App;
